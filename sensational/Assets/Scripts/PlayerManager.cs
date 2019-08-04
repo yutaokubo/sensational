f546ec7a7ad9c1b9f14ab950b2263d71f9e24980
kazuma <17cu0235@gmail.com>
2019-08-05 07:14:58 +0900

    PlayerManagerのシングルトン化

--- sensational/Assets/Scripts/PlayerManager.cs
+++ sensational/Assets/Scripts/PlayerManager.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour{
+public class PlayerManager : SingletonMono<PlayerManager>
+{
 
     //用途：プレイヤーの管理
     //作成者：大久保
     //最終更新日：2019/08/05
     //最終更新者：大久保
     //更新内容：プレイヤーが死んでいるか常に確認
+    //更新日：2019/08/05
+    //更新者:村上
+    //更新内容:getterの追加,Singleton化
 
-        
+
     [SerializeField]
     private Player player;
+    //getter
+    public Player PlayerInstance
+    {
+        get{ return player; }
+    }
 
     private bool isPlayerDead;//プレイヤーが死んでいるかどうか
 
