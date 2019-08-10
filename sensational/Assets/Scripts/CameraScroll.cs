6ae623b1e4b49796e5113633b62aecf4957449f8
KPC-017A1644\K017A1644 <yutaokubo@i.softbank.jp>
2019-08-10 19:41:57 +0900

    カメラのスクロール作成

---
+++ sensational/Assets/Scripts/CameraScroll.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScroll : MonoBehaviour {
+
+    private Player player;
+
+    [SerializeField, Header("カメラの高さ")]
+    private float y;
+
+    [SerializeField,Header("左側のスクロール制限")]
+    private float leftLimit;
+    [SerializeField, Header("右側のスクロール制限")]
+    private float rightLimit;
+
+	// Use this for initialization
+	void Start () {
+        player = PlayerManager.Instance.PlayerInstance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Scroll();
+        Limit();
+	}
+
+    void Scroll()
+    {
+        transform.position = new Vector3(player.transform.position.x, y, -10);
+    }
+
+    void Limit()
+    {
+        if(transform.position.x<=leftLimit)
+        {
+            transform.position = new Vector3(leftLimit, y, -10);
+        }
+        if(transform.position.x >=rightLimit)
+        {
+            transform.position = new Vector3(rightLimit, y, -10);
+        }
+    }
+}
